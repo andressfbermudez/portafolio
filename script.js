9b0888991514d5a135a1b2cea87ad75cb65afd31
Andres-Bermudez <Andres201900@outlook.com>
2024-07-09 23:54:13 -0500

    Corrigiendo Errores

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let menuHamburguesa = document.getElementById("menuHamburguesa");
 let menuNavegacion = document.getElementById("menuNavegacion");
 let cerrarMenu = document.getElementById("cerrarMenu");
+let imagen = document.querySelectorAll("#imagen");
+
+/*
 let seccionInicio = document.getElementById("seccionInicio");
 let seccionSobreMi = document.getElementById("seccionSobreMi");
-let seccionProyectos = document.getElementById("seccionProyectos");
+let seccionProyectos = document.getElementById("seccionProyectos");                 // Zona de conflicto: por corregir
 let seccionCertificaciones = document.getElementById("seccionCertificaciones");
-let imagen = document.querySelectorAll("#imagen");
+*/
 
 function desplegarMenuNavegacion() {
     menuNavegacion.style.display = "flex";
@@ -17,10 +20,13 @@ function cerrarMenuNavegacion() {
 
 menuHamburguesa.addEventListener("click", desplegarMenuNavegacion);
 cerrarMenu.addEventListener("click", cerrarMenuNavegacion)
+
+/*
 seccionInicio.addEventListener("click", cerrarMenuNavegacion);
 seccionSobreMi.addEventListener("click", cerrarMenuNavegacion);
-seccionProyectos.addEventListener("click", cerrarMenuNavegacion);
+seccionProyectos.addEventListener("click", cerrarMenuNavegacion);                // Zona de conflicto: Por corregir
 seccionCertificaciones.addEventListener("click", cerrarMenuNavegacion);
+*/
 
 // API para abrir imagenes en pantalla completa
 imagen.forEach(img => {
